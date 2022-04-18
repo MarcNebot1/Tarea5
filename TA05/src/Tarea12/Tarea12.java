9926a14fcf6da2b7d0026abc84515ff076039082
MarcNebot1git commit -m https://github.com/MarcNebot1/MarcNebot1.git <newgate3211@gmail.com>
2022-04-18 15:52:37 +0200

    ultimos cambios

--- TA05/src/Tarea12/Tarea12.java
+++ TA05/src/Tarea12/Tarea12.java
@@ -1,13 +1,33 @@
 package Tarea12;
 
+import java.util.Scanner;
+
 public class Tarea12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		final String CACAUETE;
+		final String somestring="a";
 		
-		
+		  Scanner sc = new Scanner(System.in);
+			
+			for(int i=0;i<=2;i++) {
+				
+				System.out.print("Introduce la contraseña ");
+				
+				String contra=sc.nextLine();
+				
+				if (contra.equalsIgnoreCase(somestring)){
+					System.out.println("la contraseña es correcta");
+					break;
+				}else {
+					System.out.println("la contraseña es incorrecta");
+					
+					
+					
+				}
+				
+			}
 		
 	}
 
